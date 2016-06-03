12b81e0898c3738d9db0ca2bfb8205d4386fc09a
Alex Peterson <alexander.peterson89@gmail.com>
2016-06-03 10:11:57 -0600

    Updating python stuff

--- python/itenerarytest.py
+++ python/itenerarytest.py
@@ -1,14 +1,15 @@
 
+######this is for the itenerary item
 
 from gapipy import Client
-
 
 APIKEY = 'live_ba297406f66b04879ccb159952ab1ee3d3726002'
 
 gapi = Client(application_key=APIKEY)
 
-
-iteneraryId =  gapi.tour_dossiers.get(24309).structured_itineraries[0].id
+###add this to database as TourId
+tourId  = 24309
+iteneraryId =  gapi.tour_dossiers.get(tourId).structured_itineraries[0].id
 
 print "=-=-=-=-=-=-=-=-=-=-=-Itenerary information =-=-=-=-=-=-=-=-=-=-=-=-="
 print gapi.itineraries.get(iteneraryId).to_json()
@@ -17,8 +18,10 @@ print "=-=-=-=-=-=-=-=-=-=-=-Itenerary information =-=-=-=-=-=-=-=-=-=-=-=-="
 daysArray =  gapi.itineraries.get(iteneraryId).days
 # days = [#, description]
 for d in daysArray:
+    #AKA FOR EACH ITIN ITEM
     print "=-=-=-=newday=-=-=-="
     print d.day
+    #rec day to database as DAY
     for component in d.components:
         #USE THE LONGEST DESCRIPTION
         summaryUnicode =  component.summary
@@ -30,6 +33,7 @@ for d in daysArray:
             if len(summaryString) > longest:
                 longest = len(summaryString)
                 stringToUse = summaryString
+                ###rec stringToUse to database, as iten. desc
             print summaryString
         except UnicodeEncodeError:
                 print "python made a mistake"
