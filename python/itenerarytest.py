e9c909d0067425b8298050b016ad60696bc93879
Alex Peterson <alexander.peterson89@gmail.com>
2016-06-03 09:31:52 -0600

    description, itenerary NUMBER, and day number are being recorded

--- python/itenerarytest.py
+++ python/itenerarytest.py
@@ -6,4 +6,46 @@ from gapipy import Client
 APIKEY = 'live_ba297406f66b04879ccb159952ab1ee3d3726002'
 
 gapi = Client(application_key=APIKEY)
-print gapi.itineraries.get(868).to_json()
+
+
+iteneraryId =  gapi.tour_dossiers.get(24309).structured_itineraries[0].id
+
+print "=-=-=-=-=-=-=-=-=-=-=-Itenerary information =-=-=-=-=-=-=-=-=-=-=-=-="
+print gapi.itineraries.get(iteneraryId).to_json()
+
+print "=-=-=-=-=-=-=-=-=-=-=-Itenerary information =-=-=-=-=-=-=-=-=-=-=-=-="
+daysArray =  gapi.itineraries.get(iteneraryId).days
+# days = [#, description]
+for d in daysArray:
+    print "=-=-=-=newday=-=-=-="
+    print d.day
+    for component in d.components:
+        #USE THE LONGEST DESCRIPTION
+        summaryUnicode =  component.summary
+        longest = 0
+        stringToUse = ""
+        try:
+            summaryString = summaryUnicode.decode("utf-8")
+            # print len(unicode_string)
+            if len(summaryString) > longest:
+                longest = len(summaryString)
+                stringToUse = summaryString
+            print summaryString
+        except UnicodeEncodeError:
+                print "python made a mistake"
+
+
+
+print "dayNumber"
+# print dayNumberOneTest
+
+
+
+#image stuff
+images = gapi.tour_dossiers.get(24319).images
+
+
+print "=-=-=-=-=-=-=-=-=-=-=-     Image links      =-=-=-=-=-=-=-=-=-=-=-=-="
+
+for image in images:
+    print image.values()[0]
