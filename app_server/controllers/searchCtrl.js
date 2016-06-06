a43d2a846b7fb61f8b040331064df5e328983d61
Alex Peterson <alexander.peterson89@gmail.com>
2016-06-06 14:16:36 -0600

    working on search

--- app_server/controllers/searchCtrl.js
+++ app_server/controllers/searchCtrl.js
@@ -1,23 +1,48 @@
-var passportConfig = require('../../config/passportConfig');
-var models = require('../../app_api/models');
-var bcrypt = require('bcryptjs');
-const saltRounds = 13;
+request = require ("request");
 
-// //Cookie stuff
-// var cookieParser = require('cookie-parser');
-// var credentials = require('../../credentials');
-// app.use(cookieParser(credentials.cookieSecret));
-module.exports.testPrintOut = function(req,res) {
-	//test so page will work TODO fix this:
-
-		console.log(JSON.parser(req.body).search);
-	res.render('index', {user : JSON.parse(req.user)});
+module.exports.go = function(req,res){
+    request.get('http://localhost:3000/search/go', function(error,response,body){
+        if (!error) {
+					console.log("OH WOW");
+					//TODO make this go to somewhere else
+            res.render("tours", {Tours : "lol"});
+        } else {
+            res.sendStatus(500);
+        }
+    })
 };
 
 
 
-module.exports.test = function(req,res) {
-	//test so page will work TODO fix this:
-		console.log("in search test")
-	res.render('index', {user : req.user});
-};
+
+
+
+
+
+
+
+// var passportConfig = require('../../config/passportConfig');
+// var models = require('../../app_api/models');
+// var bcrypt = require('bcryptjs');
+// const saltRounds = 13;
+//
+// // //Cookie stuff
+// // var cookieParser = require('cookie-parser');
+// // var credentials = require('../../credentials');
+// // app.use(cookieParser(credentials.cookieSecret));
+// module.exports.testPrintOut = function(req,res) {
+// 	//test so page will work TODO fix this:
+//
+// 		console.log(req.body.search);
+// 	res.render('index', {user : req.user});
+//
+//
+// };
+//
+//
+//
+// module.exports.test = function(req,res) {
+// 	//test so page will work TODO fix this:
+// 		console.log("in search test")
+// 	res.render('index', {user : req.user});
+// };
