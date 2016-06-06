3f51420729ce0e8520ba03a3a3f96af0c1affd36
Alex Peterson <alexander.peterson89@gmail.com>
2016-06-06 16:10:57 -0600

    working on search and fixing database dates, as well as fixing tour descriptions

--- app_server/routes/searchRoutes.js
+++ app_server/routes/searchRoutes.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var searchCtrl = require('../controllers/searchCtrl');
 
-router.post('/',searchCtrl.go);
+router.get('/',searchCtrl.go);
+router.get('/:search',searchCtrl.go);
+
 // router.post('/test', searchCtrl.test);
 
 module.exports = router;
