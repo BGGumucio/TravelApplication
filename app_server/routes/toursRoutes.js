3d71cc40db131392e2c855a4a756a7faedd1d921
Alex Peterson <alexander.peterson89@gmail.com>
2016-06-07 10:12:31 -0600

    working on add to cart / new database dump file in new directory sqlDumpFiles

--- app_server/routes/toursRoutes.js
+++ app_server/routes/toursRoutes.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var viewCtrl = require('../controllers/viewCtrl');
-
+var cartCtrl = require('../controllers/cartCtrl');
 router.get('/AllTours', viewCtrl.getAll);
 router.get('/:id', viewCtrl.getOne);
+//add to cart
+//this has to go here because it is accessed through /tours
+router.post('/cart/addToCart', cartCtrl.add);
+
 
 module.exports = router;
