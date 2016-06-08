94e873658d747f11e0f001bf6651b311e3f1b3b8
bruno <batrillo@gmail.com>
2016-06-08 15:21:25 -0600

    changed my bookings to a form

--- app_server/controllers/cartCtrl.js
+++ app_server/controllers/cartCtrl.js
@@ -7,21 +7,21 @@ const saltRounds = 13;
 // var cookie = require('cookie-parser');
 
 module.exports.add = function(req,res) {
-	var tourList = [];
-	console.log(req.body.tourId);
-	console.log(req.signedCookies.currentUser);
-	if(req.signedCookies.currentUser){
+    var tourList = [];
+    console.log(req.body.tourId);
+    console.log(req.signedCookies.currentUser);
+    if(req.signedCookies.currentUser){
 
-		var userCookie = req.signedCookies.currentUser;
+        var userCookie = req.signedCookies.currentUser;
 
-		var userName = req.signedCookies.currentUser.user.username;
-		
-		var tourID = req.body.tourId;
-		
-		var currentDate = new Date();
-		
-		console.log("before request post");
-		request.post({
+        var userName = req.signedCookies.currentUser.user.username;
+
+        var tourID = req.body.tourId;
+
+        var currentDate = new Date();
+
+        console.log("before request post");
+        request.post({
         url: 'http://localhost:3000/api/bookings/createBooking',
         headers: {
             'Content-type': 'application/json'
@@ -38,46 +38,46 @@ module.exports.add = function(req,res) {
             res.sendStatus(200);
         }
         else{
-        	console.log(response.statusCode);
+            console.log(response.statusCode);
         }
-    });				
-		
-		// var userJSON = JSON.parse(userCookie);
-		// var cookieStuff = cookie(userCookie);
-		// var userName = req.signedCookies.currentUser.username;
-		console.log(req.user);
+    });
 
-		console.log('book stuff with user username: '+ userName +'  and tourid: ' + req.body.tourId );
-		//after book logged in
-		res.redirect("/");
-		//res.render('index', {user : req.user});
+        // var userJSON = JSON.parse(userCookie);
+        // var cookieStuff = cookie(userCookie);
+        // var userName = req.signedCookies.currentUser.username;
+        console.log(req.user);
 
-	}else{
-		//after book not logged in
-		//go to logged in page
-		res.render('index', {user : req.user});
-		console.log('USER NOT FOUND');
+        console.log('book stuff with user username: '+ userName +'  and tourid: ' + req.body.tourId );
+        //after book logged in
+        res.redirect("/");
+        //res.render('index', {user : req.user});
 
-	}
+    }else{
+        //after book not logged in
+        //go to logged in page
+        res.render('index', {user : req.user});
+        console.log('USER NOT FOUND');
 
-	// if (req.signedCookies.cart.tourList){
-	// 		console.log("TOUR LIST ALREAY EXISTS");
+    }
+
+    // if (req.signedCookies.cart.tourList){
+    //         console.log("TOUR LIST ALREAY EXISTS");
   //       var cart = req.signedCookies.cart;
-	// 			tourList = cart.tourList;
-	// 			console.log("TOUR LIST: " + tourList);
-	//
+    //             tourList = cart.tourList;
+    //             console.log("TOUR LIST: " + tourList);
+    //
   //       tourList.push(req.body.tourId);
-	//
-	// 			console.log("TOUR LIST: " + tourList);
-	//
-	// 			res.cookie('cart', tourList , {signed : true});
+    //
+    //             console.log("TOUR LIST: " + tourList);
+    //
+    //             res.cookie('cart', tourList , {signed : true});
   //       // res.send(tourList);
-	//
+    //
   //   } else {
-	// 		//here
-	// 		console.log("TOUR LIST DOES NOT ALREAY EXISTS");
-	// 		tourList.push(req.body.tourId);
-	//
+    //         //here
+    //         console.log("TOUR LIST DOES NOT ALREAY EXISTS");
+    //         tourList.push(req.body.tourId);
+    //
   //       res.cookie('cart', tourList, {signed : true});
   //       // res.send(tourList);
   //   }
@@ -101,10 +101,10 @@ module.exports.add = function(req,res) {
 // // app.use(cookieParser(credentials.cookieSecret));
 //
 // module.exports.test = function(req,res) {
-// 	//test so page will work TODO fix this:
-// 		console.log('wtf');
+//     //test so page will work TODO fix this:
+//         console.log('wtf');
 //
-// 	res.send('index');
+//     res.send('index');
 //
 //
 //
@@ -113,8 +113,8 @@ module.exports.add = function(req,res) {
 //
 //
 // module.exports.addToCart = function(req,res) {
-// 	//TODO add stuff for a real tour item
-// 		console.log('sdflkjfjlk;sasdfakjkjl;fd');
+//     //TODO add stuff for a real tour item
+//         console.log('sdflkjfjlk;sasdfakjkjl;fd');
 //     res.cookie('testcookie',{tour:"tour test thing"},{signed:true});
-// 	res.send('index');
+//     res.send('index');
 // };
