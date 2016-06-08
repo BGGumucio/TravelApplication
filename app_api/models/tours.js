8184c33501c33582724c63aa2e72b3a2f1181557
Alex Peterson <alexander.peterson89@gmail.com>
2016-06-08 11:45:23 -0600

    added cost to tour model

--- app_api/models/tours.js
+++ app_api/models/tours.js
@@ -4,9 +4,10 @@ module.exports = function(sequelize, DataTypes) {
         map_image : DataTypes.STRING,
         num_avail : DataTypes.INTEGER,
         tour_image : DataTypes.STRING,
-        tour_description : DataTypes.STRING, 
+        tour_description : DataTypes.STRING,
         start_location : DataTypes.STRING,
         end_location : DataTypes.STRING,
+        cost: DataTypes.STRING,
         start_date : DataTypes.STRING,
         end_date : DataTypes.STRING
     }, {
