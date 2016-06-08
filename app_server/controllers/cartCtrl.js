3bcb39d67106a0fb8cfc210fbb8621dc79512aeb
Alex Peterson <alexander.peterson89@gmail.com>
2016-06-08 14:20:47 -0600

    fixing merge conflicts kind of

--- app_server/controllers/cartCtrl.js
+++ app_server/controllers/cartCtrl.js
@@ -7,81 +7,77 @@ const saltRounds = 13;
 // var cookie = require('cookie-parser');
 
 module.exports.add = function(req,res) {
-	var tourList = [];
-	console.log(req.body.tourId);
-	console.log(req.signedCookies.currentUser);
-	if(req.signedCookies.currentUser){
+    var tourList = [];
+    console.log(req.body.tourId);
+    console.log(req.signedCookies.currentUser);
+    if(req.signedCookies.currentUser){
 
-		var userCookie = req.signedCookies.currentUser;
+        var userCookie = req.signedCookies.currentUser;
 
-		var userName = req.signedCookies.currentUser.user.username;
+        var userName = req.signedCookies.currentUser.user.username;
 
-		var tourID = req.body.tourId;
+        var tourID = req.body.tourId;
 
-		var currentDate = new Date();
+        var currentDate = new Date();
 
-		console.log("before request post");
-		request.post({
+        console.log("before request post");
+        request.post({
         url: 'http://localhost:3000/api/bookings/createBooking',
         headers: {
-				})
-			  	.catch(function(err){		  	.catch(function(err){
-			  		console.error(err);		  		console.error(err);
-			 -		res.status(500);
-			  		res.send(err);		  		res.send(err);
-			  	});		  	});
-			  };		  };
-			 @@ -39,13 +38,11 @@ module.exports.createBooking = function(req,res) {
-			      console.log(booking);		      console.log(booking);
-			      models.Booking.create(booking)		      models.Booking.create(booking)
-			          .then(function(bookings){		          .then(function(bookings){
-			 -            res.sendStatus(201);		 +            res.render('index');
-			          })		          })
-			          .catch(function(err){		          .catch(function(err){
-			 -            res.status(500);
-			              res.send(err);		              res.send(err);
-			          });		          });
+            'Content-type': 'application/json'
+        },
+        json: {
+            user_username: userName,
+            tour_id: tourID,
+            paid_status : "PAID",
+            date_of_booking : currentDate
+        }
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 201) {
+            res.locals.flash = 'Thank you for booking with Aviato!'
+            res.sendStatus(200);
+        }
+        else{
+            console.log(response.statusCode);
+        }
+    });
 
-			  				//redirect to index TODO change to thank you page or something?		  				//redirect to index TODO change to thank you page or something?
-			 -				res.render('index');
-			  };		  };
+        // var userJSON = JSON.parse(userCookie);
+        // var cookieStuff = cookie(userCookie);
+        // var userName = req.signedCookies.currentUser.username;
+        console.log(req.user);
 
-		// var userJSON = JSON.parse(userCookie);
-		// var cookieStuff = cookie(userCookie);
-		// var userName = req.signedCookies.currentUser.username;
-		console.log(req.user);
+        console.log('book stuff with user username: '+ userName +'  and tourid: ' + req.body.tourId );
+        //after book logged in
+        res.redirect("/");
+        //res.render('index', {user : req.user});
 
-		console.log('book stuff with user username: '+ userName +'  and tourid: ' + req.body.tourId );
-		//after book logged in
-		res.redirect("/");
-		//res.render('index', {user : req.user});
+    }else{
+        //after book not logged in
+        //go to logged in page
+        res.render('index', {user : req.user});
+        console.log('USER NOT FOUND');
 
-	}else{
-		//after book not logged in
-		//go to logged in page
-		res.render('index', {user : req.user});
-		console.log('USER NOT FOUND');
+    }
 
-	}
-
-	// if (req.signedCookies.cart.tourList){
-	// 		console.log("TOUR LIST ALREAY EXISTS");
+    // if (req.signedCookies.cart.tourList){
+    //         console.log("TOUR LIST ALREAY EXISTS");
   //       var cart = req.signedCookies.cart;
-	// 			tourList = cart.tourList;
-	// 			console.log("TOUR LIST: " + tourList);
-	//
+    //             tourList = cart.tourList;
+    //             console.log("TOUR LIST: " + tourList);
+    //
   //       tourList.push(req.body.tourId);
-	//
-	// 			console.log("TOUR LIST: " + tourList);
-	//
-	// 			res.cookie('cart', tourList , {signed : true});
+    //
+    //             console.log("TOUR LIST: " + tourList);
+    //
+    //             res.cookie('cart', tourList , {signed : true});
   //       // res.send(tourList);
-	//
+    //
   //   } else {
-	// 		//here
-	// 		console.log("TOUR LIST DOES NOT ALREAY EXISTS");
-	// 		tourList.push(req.body.tourId);
-	//
+    //         //here
+    //         console.log("TOUR LIST DOES NOT ALREAY EXISTS");
+    //         tourList.push(req.body.tourId);
+    //
   //       res.cookie('cart', tourList, {signed : true});
   //       // res.send(tourList);
   //   }
@@ -105,10 +101,10 @@ module.exports.add = function(req,res) {
 // // app.use(cookieParser(credentials.cookieSecret));
 //
 // module.exports.test = function(req,res) {
-// 	//test so page will work TODO fix this:
-// 		console.log('wtf');
+//     //test so page will work TODO fix this:
+//         console.log('wtf');
 //
-// 	res.send('index');
+//     res.send('index');
 //
 //
 //
@@ -117,8 +113,8 @@ module.exports.add = function(req,res) {
 //
 //
 // module.exports.addToCart = function(req,res) {
-// 	//TODO add stuff for a real tour item
-// 		console.log('sdflkjfjlk;sasdfakjkjl;fd');
+//     //TODO add stuff for a real tour item
+//         console.log('sdflkjfjlk;sasdfakjkjl;fd');
 //     res.cookie('testcookie',{tour:"tour test thing"},{signed:true});
-// 	res.send('index');
+//     res.send('index');
 // };
